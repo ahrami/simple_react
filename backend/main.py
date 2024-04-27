0fb0a3a600c90d22aa5c4f1ec61a99803231cc6f
ahrami <andrxai1@yandex.ru>
2024-04-27 14:55:20 +0300

    simplest backend finished

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
+class Counter():
+    count: int = 0
+    
+counter = Counter()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"count": counter.count}
+
+class CounterBody(BaseModel):
+    value: int = Field(0, ge=0)
+
+@app.post("/add")
+async def add(body: CounterBody):
+    counter.count += body.value
+    return {"count": counter.count}
+
+@app.post("/subtract")
+async def subtract(body: CounterBody):
+    counter.count -= body.value
+    return {"count": counter.count}
