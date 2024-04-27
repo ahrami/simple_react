c771aa8764b5cddf0c1b9e9cf00063a34e663e0b
ahrami <andrxai1@yandex.ru>
2024-04-27 19:29:55 +0300

    CORS middleware and time.sleep on requests

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
+from fastapi.middleware.cors import CORSMiddleware
+
+import time
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Counter():
     count: int = 0
@@ -10,6 +21,7 @@ counter = Counter()
 
 @app.get("/")
 async def root():
+    time.sleep(0.2)
     return {"count": counter.count}
 
 class CounterBody(BaseModel):
@@ -17,10 +29,12 @@ class CounterBody(BaseModel):
 
 @app.post("/add")
 async def add(body: CounterBody):
+    time.sleep(0.2)
     counter.count += body.value
     return {"count": counter.count}
 
 @app.post("/subtract")
 async def subtract(body: CounterBody):
+    time.sleep(0.2)
     counter.count -= body.value
     return {"count": counter.count}
